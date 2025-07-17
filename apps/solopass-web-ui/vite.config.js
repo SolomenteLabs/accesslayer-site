757b9dd99936ca7024ad75567b34221715fe41d3
SolomenteLabs <scrt13@protonmail.com>
2025-07-17 02:47:30 -0600

    🚀 Add /demo-v5 build to public folder

--- apps/solopass-web-ui/vite.config.js
+++ apps/solopass-web-ui/vite.config.js
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
-    },
-  },
-  define: {
-    'process.env': {},
+  base: '/demo-v5/',
+  build: {
+    outDir: 'dist',
   },
 })
-
